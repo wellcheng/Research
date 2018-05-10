8cae6c825e4375b5651a381a60cc6c52574453ae
wellcheng <326955439@qq.com>
2018-05-11 00:15:34 +0800

    RACChannel

--- ResearchPods/RSReactiveObjC/ReactiveObjC/RACChannel.m
+++ ResearchPods/RSReactiveObjC/ReactiveObjC/RACChannel.m
@@ -30,13 +30,18 @@
 
 	// We don't want any starting value from the leadingSubject, but we do want
 	// error and completion to be replayed.
+    // leading 是指保存一个的 replay
 	RACReplaySubject *leadingSubject = [[RACReplaySubject replaySubjectWithCapacity:0] setNameWithFormat:@"leadingSubject"];
+    // follow 保存一个
 	RACReplaySubject *followingSubject = [[RACReplaySubject replaySubjectWithCapacity:1] setNameWithFormat:@"followingSubject"];
 
 	// Propagate errors and completion to everything.
+    // leading 只发送 error 到 follow
 	[[leadingSubject ignoreValues] subscribe:followingSubject];
+    // follow 发送 error 和 complete 到 leading
 	[[followingSubject ignoreValues] subscribe:leadingSubject];
 
+    // 两个之间相互打通
 	_leadingTerminal = [[[RACChannelTerminal alloc] initWithValues:leadingSubject otherTerminal:followingSubject] setNameWithFormat:@"leadingTerminal"];
 	_followingTerminal = [[[RACChannelTerminal alloc] initWithValues:followingSubject otherTerminal:leadingSubject] setNameWithFormat:@"followingTerminal"];
 
